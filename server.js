b6bdf6ac7cd6bf53c3266c796b8f3139f4f4bea7
Hari Balajee K <haritrance28@gamil.com>
2025-03-08 23:35:51 +0530

    second change as cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: "https://your-app.vercel.app" }))
 
 //mongodb database
 
